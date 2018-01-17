ea5edd424aa57ea026eb61c0b68c211af7c8a1c1
Manuel Lema <ManuelPro@Manuels-MacBook-Pro.local>
2018-01-16 19:23:51 -0500

    new changes

--- components/com_helloworld/admin/sql/install.mysql.utf8.sql
+++ components/com_helloworld/admin/sql/install.mysql.utf8.sql
@@ -1,15 +1,20 @@
-DROP TABLE IF EXISTS `#__helloworld`;
+DROP TABLE IF EXISTS `#__website_specials`;
 
-CREATE TABLE `#__helloworld` (
-	`id`       INT(11)     NOT NULL AUTO_INCREMENT,
-	`greeting` VARCHAR(25) NOT NULL,
-	`published` tinyint(4) NOT NULL DEFAULT '1',
+CREATE TABLE `#__website_specials` (
+	`id`       				INT(11)     NOT NULL AUTO_INCREMENT,
+	`created_by`    		INT(11)     NOT NULL,
+	`modified_by`   		INT(11)     NOT NULL,
+	`start_date`    		DATETIME    NOT NULL,
+	`end_date` 	    		DATETIME    NOT NULL,
+	`dollar_discount` 	    DOUBLE	    NOT NULL,
+	`percentage_discount` 	DOUBLE      NOT NULL,
+	`week` 					DATE        NOT NULL,
+	`special_name` 	 		VARCHAR(255)NOT NULL,
+	`discount_type` 		VARCHAR(255)NOT NULL,
+	`notes` 				TEXT        NOT NULL,
+	`published` 	tinyint(4) NOT NULL DEFAULT '1',
 	PRIMARY KEY (`id`)
 )
 	ENGINE =MyISAM
 	AUTO_INCREMENT =0
 	DEFAULT CHARSET =utf8;
-
-INSERT INTO `#__helloworld` (`greeting`) VALUES
-('Hello World!'),
-('Good bye World!');
